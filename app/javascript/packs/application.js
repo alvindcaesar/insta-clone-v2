c9cf84ac88be36ddcf5f3ec0fbf2a27c3a86a187
alvindcaesar <alvind.caesar@gmail.com>
2021-07-29 16:08:41 +0800

    Configure TailwindCSS for PostCSS 8 support

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,11 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "stylesheets/application.scss"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
-
-import "../stylesheets/application"
-import "../modules/formValidation"
