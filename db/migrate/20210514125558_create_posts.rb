72c42589bb0b4e65434ff6e4f804deae08e92392
alvindcaesar <alvind.caesar@gmail.com>
2021-07-29 16:05:32 +0800

    Remove unused files

--- db/migrate/20210514125558_create_posts.rb
+++
@@ -1,9 +0,0 @@
-class CreatePosts < ActiveRecord::Migration[6.1]
-  def change
-    create_table :posts do |t|
-      t.references :user, null: false, foreign_key: true
-      t.string :caption
-      t.timestamps
-    end
-  end
-end
